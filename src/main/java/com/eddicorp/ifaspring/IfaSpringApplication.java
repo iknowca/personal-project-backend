079801c6d44ec5879e46d86bdc3f728532fc6dda
iknowca <iknowca@gmail.com>
2023-07-03 15:32:20 +0900

    init: [김원기] 스프링 저장소 초기 세팅 [IFA-SPRING-1]

---
+++ src/main/java/com/eddicorp/ifaspring/IfaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.eddicorp.ifaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfaSpringApplication.class, args);
+	}
+
+}
