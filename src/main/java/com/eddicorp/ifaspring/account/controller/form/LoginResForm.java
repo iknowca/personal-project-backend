93d19629b04399e8a1d48a57d1fd630df28f11fb
iknowca <iknowca@gmail.com>
2023-07-13 09:59:29 +0900

    update: [김원기] 로그인시 유저닉네임을 추가로 보낸다 [IFA-SPRING-4]

--- src/main/java/com/eddicorp/ifaspring/account/controller/form/LoginResForm.java
+++ src/main/java/com/eddicorp/ifaspring/account/controller/form/LoginResForm.java
@@ -5,8 +5,10 @@ import lombok.Getter;
 @Getter
 public class LoginResForm {
     private String userToken;
+    private String nickName;
 
-    public LoginResForm(String userToken) {
+    public LoginResForm(String userToken, String nickName) {
         this.userToken = userToken;
+        this.nickName = nickName;
     }
 }
